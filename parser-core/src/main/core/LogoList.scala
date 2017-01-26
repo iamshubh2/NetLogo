99cad6c690c54b2a26908fbc63071eca40d9caad
brotatotes <hao.awesome@gmail.com>
2017-01-25 19:20:32 -0600

    Added `insert-item` [issue #214]

--- parser-core/src/main/core/LogoList.scala
+++ parser-core/src/main/core/LogoList.scala
@@ -53,6 +53,8 @@ class LogoList private (private val v: Vector[AnyRef])
   override def reverse = new LogoList(v.reverse)
   def replaceItem(index: Int, obj: AnyRef) =
     new LogoList(v.updated(index, obj))
+  def insertItem(index: Int, obj: AnyRef, elt: AnyRef) =
+    new LogoList(v.slice(0, index) ++ Vector(elt) ++ v.slice(index, v.size))
   def logoSublist(start: Int, stop: Int) =
     new LogoList(v.slice(start, stop))
   def butFirst = new LogoList(v.tail)
